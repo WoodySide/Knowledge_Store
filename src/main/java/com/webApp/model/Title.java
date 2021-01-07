c222166a758ccc6d55ce8c36f7c96bf6399f91b6
Alex Vasyaev <alexwoodyside@gmail.com>
2021-01-07 08:41:55 +0300

    polishing

--- src/main/java/com/webApp/model/Title.java
+++ src/main/java/com/webApp/model/Title.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Entity(name = "Title")
-@Table(name = "titles", schema = "knowledge_data")
+@Table(name = "titles", schema = "knowledge_data", catalog = "knowledge_store")
 @NoArgsConstructor
 @Getter
 @Setter
