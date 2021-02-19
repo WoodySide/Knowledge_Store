bf97dba0394b7c08fb1b6eab46bd2bea3ddcb6bf
Alex Vasyaev <alexwoodyside@gmail.com>
2021-02-19 14:46:33 +0300

    add two new methods

--- src/main/java/com/webApp/repository/CategoryRepository.java
+++ src/main/java/com/webApp/repository/CategoryRepository.java
@@ -1,11 +1,19 @@
 package com.webApp.repository;
 
 import com.webApp.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Category findCategoryByName(String categoryName);
+
+    Page<Category> findByTitleId(Long titleId, Pageable pageable);
+
+    Optional<Category> findByIdAndTitleId(Long id, Long titleId);
 }
