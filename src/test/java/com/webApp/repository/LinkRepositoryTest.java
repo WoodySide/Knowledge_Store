b0011b68875778bb9b62ba0d22da6e307033056d
Alex Vasyaev <alexwoodyside@gmail.com>
2021-06-13 21:03:10 +0300

    cover LinkRepository with tests

--- src/test/java/com/webApp/repository/LinkRepositoryTest.java
+++ src/test/java/com/webApp/repository/LinkRepositoryTest.java
@@ -1,20 +1,22 @@
 package com.webApp.repository;
 
 
+import com.webApp.model.Category;
 import com.webApp.model.Link;
+import com.webApp.model.Title;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
+@RunWith(SpringRunner.class)
 public class LinkRepositoryTest {
 
     @Autowired
@@ -25,30 +27,234 @@ public class LinkRepositoryTest {
 
 
     @Test
-    public void whenFindByName_thenReturnLink() {
+    public void whenRepositoryIsEmpty_thenReturnNull() {
 
-        //given
-        Link link = Link.builder()
-                .linkName("https://www.baeldung.com/spring-requestmapping")
-                .build();
-        testEntityManager.persist(link);
-        testEntityManager.flush();
+        Iterable<Link> links = linkRepository.findAll();
 
-        //when
-        Link foundLink = linkRepository.findLinkByLinkName(link.getLinkName());
-
-        //then
-        assertThat(foundLink.getLinkName()).isEqualTo(link.getLinkName());
+        assertThat(links).isEmpty();
     }
 
 
     @Test
-    public void whenFindAll_thenReturnLinkList() {
+    public void whenFindByName_thenReturnLink() {
+
+        Title title = Title
+                .builder()
+                .name("Title")
+                .build();
+
+        Category category = Category
+                .builder()
+                .name("Category")
+                .build();
+
+        Link link = Link
+                .builder()
+                .linkName("http://LinkName")
+                .build();
+
+        category.setTitle(title);
+        link.setCategory(category);
+
+        testEntityManager.persist(link);
+
         //when
-        List<Link> linkList = linkRepository.findAll();
+        Link foundLink = linkRepository.findLinkByLinkName("http://LinkName");
 
         //then
-        assertThat(linkList).hasSize(10);
+        assertThat(foundLink.getLinkName()).isEqualTo("http://LinkName");
+    }
+
+    @Test
+    public void whenFindAll_thenReturnList() {
+
+        Title title = Title
+                .builder()
+                .name("Title")
+                .build();
+
+        Category category = Category
+                .builder()
+                .name("Category")
+                .build();
+
+        Link link = Link
+                .builder()
+                .linkName("http://LinkName")
+                .build();
+
+        category.setTitle(title);
+        link.setCategory(category);
+
+        testEntityManager.persist(link);
+
+        //when
+        List<Link> links = linkRepository.findAll();
+
+        //then
+        assertThat(links).hasSizeGreaterThan(0);
+    }
+
+    public void whenFindById_thenReturnLink() {
+
+        Title title = Title
+                .builder()
+                .name("Title")
+                .build();
+
+        Category category = Category
+                .builder()
+                .name("Category")
+                .build();
+
+        Link link = Link
+                .builder()
+                .linkName("http://LinkName")
+                .build();
+
+        category.setTitle(title);
+        link.setCategory(category);
+
+        testEntityManager.persist(link);
+
+        //when
+        Link foundLink = linkRepository.findById(link.getId()).get();
+
+        //then
+        assertThat(foundLink).isEqualTo(link);
+    }
+
+    @Test
+    public void whenDeleteLink_thenReturnNull() {
+
+        Title title1 = Title
+                .builder()
+                .name("Title1")
+                .build();
+
+        Title title2 = Title
+                .builder()
+                .name("Title2")
+                .build();
+
+        Title title3 = Title
+                .builder()
+                .name("Title3")
+                .build();
+
+        Category category1 = Category
+                .builder()
+                .name("Category1")
+                .build();
+
+        Category category2 = Category
+                .builder()
+                .name("Category2")
+                .build();
+
+        Category category3 = Category
+                .builder()
+                .name("Category3")
+                .build();
+
+        Link link1 = Link
+                .builder()
+                .linkName("http://LinkName1")
+                .build();
+
+        Link link2 = Link
+                .builder()
+                .linkName("http://LinkName2")
+                .build();
+
+        Link link3 = Link
+                .builder()
+                .linkName("http://LinkName3")
+                .build();
+
+        category1.setTitle(title1);
+        category2.setTitle(title2);
+        category3.setTitle(title3);
+        link1.setCategory(category1);
+        link2.setCategory(category2);
+        link3.setCategory(category3);
+
+        testEntityManager.persist(link1);
+        testEntityManager.persist(link2);
+        testEntityManager.persist(link3);
+
+        linkRepository.deleteById(link2.getId());
+
+        //when
+        Iterable<Link> links = linkRepository.findAll();
+
+        //then
+        assertThat(links).hasSize(2).contains(link1,link3);
+    }
+
+    @Test
+    public void whenDeleteAllLinks_thenReturnNoLinks() {
+        Title title1 = Title
+                .builder()
+                .name("Title1")
+                .build();
+
+        Title title2 = Title
+                .builder()
+                .name("Title2")
+                .build();
+
+        Title title3 = Title
+                .builder()
+                .name("Title3")
+                .build();
+
+        Category category1 = Category
+                .builder()
+                .name("Category1")
+                .build();
+
+        Category category2 = Category
+                .builder()
+                .name("Category2")
+                .build();
+
+        Category category3 = Category
+                .builder()
+                .name("Category3")
+                .build();
+
+        Link link1 = Link
+                .builder()
+                .linkName("http://LinkName1")
+                .build();
+
+        Link link2 = Link
+                .builder()
+                .linkName("http://LinkName2")
+                .build();
+
+        Link link3 = Link
+                .builder()
+                .linkName("http://LinkName3")
+                .build();
+
+        category1.setTitle(title1);
+        category2.setTitle(title2);
+        category3.setTitle(title3);
+        link1.setCategory(category1);
+        link2.setCategory(category2);
+        link3.setCategory(category3);
+
+        testEntityManager.persist(link1);
+        testEntityManager.persist(link2);
+        testEntityManager.persist(link3);
+
+        //when
+        linkRepository.deleteAll();
+
+        //then
+        assertThat(linkRepository.findAll()).isEmpty();
     }
 }
 
