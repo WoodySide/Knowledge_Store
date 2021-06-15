099c1302b6f17ba4f6292905fe591e33a4745410
Alex Vasyaev <alexwoodyside@gmail.com>
2021-06-15 21:33:46 +0300

    cover TitleController with tests

--- src/test/java/com/webApp/controller/TitleControllerTest.java
+++ src/test/java/com/webApp/controller/TitleControllerTest.java
@@ -1,29 +1,30 @@
 package com.webApp.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.webApp.model.Category;
 import com.webApp.model.Title;
 import com.webApp.service.TitleService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import javax.persistence.EntityNotFoundException;
-import java.util.*;
 
-import static org.hamcrest.Matchers.is;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(TitleController.class)
+@WebMvcTest(controllers = TitleController.class)
+@ActiveProfiles("test")
 public class TitleControllerTest {
 
     @Autowired
@@ -35,106 +36,132 @@ public class TitleControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private List<Title> titles;
 
-
-    @Test
-    public void givenAllTitles_whenGetTitles_thenStatus200andTitlesReturned() throws Exception {
-        Title title = Title.builder()
-                .name("Biology")
-                .build();
-
-        List<Title> titles = Collections.singletonList(title);
-        Mockito.when(titleService.findAllTitles()).thenReturn(titles);
-
-        mockMvc.perform(
-                get("/api/v1/titles/"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].name", is(title.getName())));
-    }
-
-    @Test
-    public void givenId_whenGetExistingTitle_thenStatus200andTitleReturned() throws Exception {
-
-        Title found = Title.builder()
-                .id(15L)
-                .name("History")
-                .build();
-        Mockito.when(titleService.findTitleById(Mockito.any())).thenReturn(Optional.of(found));
-
-        mockMvc.perform(
-                get("/api/v1/titles/{id}", found.getId()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value("15"))
-                .andExpect(jsonPath("$.name").value("History"));
-    }
-
-    @Test
-    public void givenId_whenGetNotExistingTitle_thenStatus404anExceptionThrown() throws Exception {
-
-
-        Mockito.when(titleService.findTitleById(Mockito.any())).
-                thenReturn(Optional.empty());
-
-        mockMvc.perform(
-                get("/api/v1/titles/1"))
-                .andExpect(status().isNotFound())
-                .andExpect(mvcResult -> mvcResult.getResolvedException()
-                        .getClass()
-                        .equals(EntityNotFoundException.class));
+    @BeforeEach
+    public void setUp() {
+        this.titles = new ArrayList<>();
+        this.titles.add(Title.builder().id(1L).name("Title1").build());
+        this.titles.add(Title.builder().id(2L).name("Title2").build());
+        this.titles.add(Title.builder().id(3L).name("Title3").build());
 
     }
 
     @Test
-    public void givenTitle_whenAdd_thenStatus201AndTitleReturned() throws Exception {
-        Set<Category> categories = new HashSet<>();
-        Title titleToBeAdded = Title.builder()
-                .categories(categories)
-                .name("Computer Science")
-                .build();
+    public void whenFetchAllTitle_thenReturnThem() throws Exception {
 
-        Mockito.when(titleService.saveTitle(Mockito.any())).thenReturn(titleToBeAdded);
+        given(titleService.findAllTitles()).willReturn(titles);
 
-        mockMvc.perform(
-                post("/api/v1/titles/")
-                .content(objectMapper.writeValueAsString(titleToBeAdded))
-                .contentType(MediaType.APPLICATION_JSON)
-        )
-                .andExpect(status().isCreated()).andExpect(content().json(objectMapper.writeValueAsString(titleToBeAdded)));
+        this.mockMvc.perform(get("/api/v1/titles/"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.size()", is(titles.size())));
     }
 
     @Test
-    public void givenTitle_whenDeleteTitle_thenStatus204() throws Exception {
+    public void whenFetchTitleById_thenReturnTitle() throws Exception {
 
-        Title titleToBeDeleted = Title.builder()
+        final Long titleId = 1L;
+        final Title title = Title.builder()
                 .id(1L)
-                .name("Archeology")
+                .name("Title")
                 .build();
 
-        Mockito.when(titleService.findTitleById(Mockito.any())).thenReturn(Optional.of(titleToBeDeleted));
+        given(titleService.findTitleById(titleId)).willReturn(Optional.of(title));
 
-        mockMvc.perform(
-                delete("/api/v1/titles/1"))
-                .andExpect(status().isNoContent());
+        this.mockMvc.perform(get("/api/v1/titles/{id}", titleId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(title.getName())));
+    }
+
+    @Test
+    public void whenNonExistingTitleFound_thenReturn404() throws Exception {
+
+        final Long titleId = 1L;
+
+        given(titleService.findTitleById(titleId)).willReturn(Optional.empty());
+
+        this.mockMvc.perform(get("/api/v1/titles/{id}", titleId))
+                .andExpect(status().isNotFound());
 
     }
 
     @Test
-    public void givenTitle_whenUpdateTitle_thenStatus204() throws Exception {
+    public void whenCreateTitle_thenReturnCreatedOne() throws Exception {
 
+        given(titleService.saveTitle(any(Title.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
 
-        Title titleToBeUpdated = Title.builder()
-                .id(15L)
-                .name("IT")
+        Title title = Title.builder()
+                .id(null)
+                .name("Title")
                 .build();
 
-        Mockito.when(titleService.saveTitle(Mockito.any())).thenReturn(titleToBeUpdated);
-        Mockito.when(titleService.findTitleById(Mockito.any())).thenReturn(Optional.of(titleToBeUpdated));
+        this.mockMvc.perform(post("/api/v1/titles/")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(title)))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.name", is(title.getName())));
+    }
 
-        mockMvc.perform(
-                put("/api/v1/titles/{id}", titleToBeUpdated.getId())
-                        .content(objectMapper.writeValueAsString(new Title("Biology")))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNoContent());
+    @Test
+    public void whenCreateNewUserWithoutName_thenReturn404() throws Exception {
+
+        Title title = Title.builder()
+                .id(null)
+                .name(null)
+                .build();
+
+        this.mockMvc.perform(post("/api/v1/titles/")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(title)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(header().string("Content-Type", is("application/json")))
+                    .andReturn();
+    }
+
+    @Test
+    public void whenUpdateNonExistingTitle_thenReturnU404() throws Exception {
+
+        Long titleId = 1L;
+
+        given(titleService.findTitleById(titleId)).willReturn(Optional.empty());
+
+        Title title = Title.builder()
+                .id(titleId)
+                .name("Title")
+                .build();
+
+        this.mockMvc.perform(put("/api/v1/titles/{id}", titleId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(title)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void whenDeleteTitle_thenReturnNone() throws Exception {
+
+        Long titleId = 1L;
+        Title title = Title.builder()
+                .id(titleId)
+                .name("Title")
+                .build();
+
+        given(titleService.findTitleById(titleId)).willReturn(Optional.of(title));
+        doNothing().when(titleService).deleteTitleById(title.getId());
+
+        this.mockMvc.perform(delete("/api/v1/titles/{id}", title.getId()))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+    }
+
+    @Test
+    public void whenDeleteNonExistingTitle_thenReturn404() throws Exception {
+
+        Long titleId = 1L;
+        given(titleService.findTitleById(titleId)).willReturn(Optional.empty());
+
+        this.mockMvc.perform(delete("/api/v1/titles/{id}", titleId))
+                .andExpect(status().isNotFound());
     }
 }
 
