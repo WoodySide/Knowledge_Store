5eb1d0679fe38e7dd7afd3b05fe7400201034877
Alex Vasyaev <alexwoodyside@gmail.com>
2021-06-15 21:32:34 +0300

    create JpaAuditConfig class to log creation and update time of entities

--- src/main/java/com/webApp/config/JpaAuditConfig.java
+++ src/main/java/com/webApp/config/JpaAuditConfig.java
@@ -1,2 +1,9 @@
-package com.webApp.config;public class JpaAuditConfig {
+package com.webApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
 }
