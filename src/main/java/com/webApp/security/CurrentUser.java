2dc4ef6e7769a36a61385378beb0c5975df23c18
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-02 15:21:05 +0300

    add security classes

--- src/main/java/com/webApp/security/CurrentUser.java
+++ src/main/java/com/webApp/security/CurrentUser.java
@@ -1,2 +1,12 @@
-package com.webApp.security;public class CurrentUser {
+package com.webApp.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
 }
