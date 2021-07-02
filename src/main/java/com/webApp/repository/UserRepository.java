69d886e0f8bd5261e5551ad159ac26b354ca78ce
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-02 15:18:12 +0300

    add UserRepository

--- src/main/java/com/webApp/repository/UserRepository.java
+++ src/main/java/com/webApp/repository/UserRepository.java
@@ -1,2 +1,26 @@
-package com.webApp.repository;public class UserRepository {
+package com.webApp.repository;
+
+import com.webApp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Optional<User> findByEmail(String email);
+
+    List<User> findByIdIn(List<Long> userIds);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+
 }
