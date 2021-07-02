0d3afe0cf302ddac502e8a6818c99e04c4864068
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-02 15:10:21 +0300

    UserController class

--- src/main/java/com/webApp/controller/UserController.java
+++ src/main/java/com/webApp/controller/UserController.java
@@ -1,2 +1,52 @@
-package com.webApp.controller;public class UserController {
+package com.webApp.controller;
+
+import com.webApp.exception_handling.NoSuchEntityException;
+import com.webApp.model.User;
+import com.webApp.payload.*;
+import com.webApp.repository.TitleRepository;
+import com.webApp.repository.UserRepository;
+import com.webApp.security.CurrentUser;
+import com.webApp.security.UserPrincipal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TitleRepository titleRepository;
+
+    @GetMapping(path = "/user/me")
+    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
+        return userSummary;
+    }
+
+    @GetMapping(path = "/user/checkUsernameAvailability")
+    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
+        Boolean isAvailable = !userRepository.existsByUsername(username);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping(path = "/user/checkEmailAvailability")
+    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
+        Boolean isAvailable = !userRepository.existsByEmail(email);
+        return new UserIdentityAvailability(isAvailable);
+    }
+
+    @GetMapping(path = "/users/{username}")
+    public UserProfile getUserProfile(@PathVariable(value = "username") String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new NoSuchEntityException("User not found"));
+
+        UserProfile userProfile = new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getCreatedAt());
+
+        return userProfile;
+    }
 }
