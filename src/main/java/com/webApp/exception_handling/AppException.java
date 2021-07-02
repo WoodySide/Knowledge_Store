a05bd25c7f18690ea44c55fedce39786830c623a
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-02 15:12:41 +0300

    add appException for logging internal_server_error

--- src/main/java/com/webApp/exception_handling/AppException.java
+++ src/main/java/com/webApp/exception_handling/AppException.java
@@ -1,2 +1,16 @@
-package com.webApp.exception_handling;public class AppException {
+package com.webApp.exception_handling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+    public AppException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
