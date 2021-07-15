2be9a947d0fbaf4ff7d83df317f0be1798b23ce1
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:39:31 +0300

    Refactor User model

--- src/main/java/com/webApp/model/User.java
+++ src/main/java/com/webApp/model/User.java
@@ -1,6 +1,7 @@
 package com.webApp.model;
 
 import com.webApp.model.audit.AuditModel;
+import com.webApp.validation.annotation.NullOrNotBlank;
 import io.swagger.annotations.ApiModel;
 import lombok.*;
 import org.hibernate.annotations.NaturalId;
@@ -8,6 +9,7 @@ import org.hibernate.annotations.NaturalId;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
@@ -18,49 +20,148 @@ import java.util.Set;
 @AllArgsConstructor
 @Getter
 @Setter
-@ToString
 @Builder
 @ApiModel(description = "All available users")
 public class User extends AuditModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "USER_ID")
     private Long id;
 
-    @Column(name = "name")
-    @NotBlank(message = "name should not be empty")
-    @Size(max = 40)
-    private String name;
-
-    @Column(name = "username")
-    @NotBlank(message = "username should not be empty")
-    @Size(max = 20)
-    private String username;
-
-    @Column(name = "email")
+    @Column(name = "email", unique = true)
     @NaturalId
-    @NotBlank(message = "email should not be empty")
+    @NotBlank(message = "Email can not be empty")
     @Size(max = 40)
     @Email
     private String email;
 
+    @Column(name = "username", unique = true)
+    @NotBlank(message = "Username can not be empty")
+    @Size(max = 20)
+    private String username;
+
     @Column(name = "password")
-    @NotBlank(message = "password should not be empty")
+    @NotNull(message = "Password can not be null")
     @Size(max = 100)
     private String password;
 
+    @Column(name = "FIRST_NAME")
+    @NullOrNotBlank(message = "First name can not be blank")
+    private String firstName;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "user_roles",
-               joinColumns = @JoinColumn(name = "user_id"),
-               inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @Column(name = "LAST_NAME")
+    @NullOrNotBlank(message = "Last name can not be blank")
+    private String lastName;
+
+    @Column(name = "IS_ACTIVE", nullable = false)
+    private Boolean active;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "USER_AUTHORITY", joinColumns = {
+            @JoinColumn(name = "USER_ID", referencedColumnName = "USER_ID")}, inverseJoinColumns = {
+            @JoinColumn(name = "ROLE_ID", referencedColumnName = "ROLE_ID")})
     private Set<Role> roles = new HashSet<>();
 
-    public User(String name, String username, String email, String password) {
-        this.name = name;
+    @Column(name = "IS_EMAIL_VERIFIED", nullable = false)
+    private Boolean isEmailVerified;
+
+
+    public User(User user) {
+        id = user.getId();
+        username = user.getUsername();
+        password = user.getPassword();
+        firstName = user.getFirstName();
+        lastName = user.getFirstName();
+        email = user.getEmail();
+        active = user.getActive();
+        roles = user.getRoles();
+        isEmailVerified = user.getIsEmailVerified();
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+        role.getUserList().add(this);
+    }
+
+    public void addRoles(Set<Role> roles) {
+        roles.forEach(this::addRole);
+    }
+
+
+    public void markVerificationConfirmed() {
+        setEmailVerified(true);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
         this.username = username;
-        this.email = email;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> authorities) {
+        roles = authorities;
+    }
+
+    public Boolean getEmailVerified() {
+        return isEmailVerified;
+    }
+
+    public void setEmailVerified(Boolean emailVerified) {
+        isEmailVerified = emailVerified;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", email='" + email + '\'' + ", username='" + username + '\'' + ", password='"
+                + password + '\'' + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", active="
+                + active + ", roles=" + roles + ", isEmailVerified=" + isEmailVerified + '}';
+    }
 }
+
