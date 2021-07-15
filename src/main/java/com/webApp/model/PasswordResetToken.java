78a122b0e250c7ca29b5d2db7b9869ab594bba03
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:38:17 +0300

    Add PasswordResetToken.class

--- src/main/java/com/webApp/model/PasswordResetToken.java
+++ src/main/java/com/webApp/model/PasswordResetToken.java
@@ -1,2 +1,38 @@
-package com.webApp.model;public class PasswordResetToken {
+package com.webApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Table(name = "password_reset_token")
+@Entity(name = "PASSWORD_RESET_TOKEN")
+@AllArgsConstructor
+@Getter
+@Setter
+public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TOKEN_ID")
+    private Long id;
+
+    @NaturalId
+    @Column(name = "TOKEN_NAME", nullable = false, unique = true)
+    private String token;
+
+    @Column(name = "EXPIRY_DT", nullable = false)
+    private Instant expiryDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "USER_ID")
+    private User user;
+
+
+    public PasswordResetToken() {
+
+    }
 }
