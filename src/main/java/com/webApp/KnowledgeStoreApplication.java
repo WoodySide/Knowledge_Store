98fed2ba5cde28b2bf6602f36dfb90ea18934cf4
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:13:20 +0300

    Add @EntityScan

--- src/main/java/com/webApp/KnowledgeStoreApplication.java
+++ src/main/java/com/webApp/KnowledgeStoreApplication.java
@@ -2,47 +2,19 @@ package com.webApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
-@EnableSwagger2
 @EnableAspectJAutoProxy
+@EntityScan(basePackageClasses = {
+		KnowledgeStoreApplication.class,
+		Jsr310JpaConverters.class
+})
 public class KnowledgeStoreApplication {
-
-	@Bean
-	public Docket swagger() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.apiInfo(getApiInfo())
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build();
-
-	}
-
-	private ApiInfo getApiInfo() {
-		Contact contact = new Contact("Alex Vasyaev", "https://github.com/WoodySide", "alexwoodyside@gmail.com");
-		return new ApiInfoBuilder()
-				.title("Knowledge Store")
-				.description("Spring Boot RESTful APIs")
-				.version("1.0.0")
-				.license("Apache 2.0")
-				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
-				.contact(contact)
-				.build();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KnowledgeStoreApplication.class, args);
 	}
-
 }
