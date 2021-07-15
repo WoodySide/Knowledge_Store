3e95b3d9bbec173da1e5cc3a80d08e06cd104247
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:19:17 +0300

    Add Swagger Configuration

--- src/main/java/com/webApp/config/SwaggerConfig.java
+++ src/main/java/com/webApp/config/SwaggerConfig.java
@@ -1,2 +1,40 @@
-package com.webApp.config;public class SwaggerConfig {
+package com.webApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+
+    }
+
+    private ApiInfo getApiInfo() {
+        Contact contact = new Contact("Alex Vasyaev", "https://github.com/WoodySide", "alexwoodyside@gmail.com");
+        return new ApiInfoBuilder()
+                .title("Knowledge Store")
+                .description("Spring Boot RESTful APIs")
+                .version("1.0.0")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .contact(contact)
+                .build();
+    }
 }
