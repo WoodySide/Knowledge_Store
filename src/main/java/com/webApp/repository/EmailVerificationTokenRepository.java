e1f90f8eba737703192195b559bce30fba09905c
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:44:23 +0300

    Add EmailVerificationTokenRepository

--- src/main/java/com/webApp/repository/EmailVerificationTokenRepository.java
+++ src/main/java/com/webApp/repository/EmailVerificationTokenRepository.java
@@ -1,2 +1,13 @@
-package com.webApp.repository;public class EmailVerificationTokenRepository {
+package com.webApp.repository;
+
+import com.webApp.model.token.EmailVerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, Long> {
+
+    Optional<EmailVerificationToken> findByToken(String token);
 }
