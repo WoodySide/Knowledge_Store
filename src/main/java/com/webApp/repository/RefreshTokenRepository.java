e3719c6359cb50d98f0dc53426c38436bf584304
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:47:52 +0300

    Add RefreshTokenRepository

--- src/main/java/com/webApp/repository/RefreshTokenRepository.java
+++ src/main/java/com/webApp/repository/RefreshTokenRepository.java
@@ -1,2 +1,18 @@
-package com.webApp.repository;public class RefreshTokenRepository {
+package com.webApp.repository;
+
+import com.webApp.model.token.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    @Override
+    Optional<RefreshToken> findById(Long id);
+
+    Optional<RefreshToken> findByToken(String token);
+
 }
