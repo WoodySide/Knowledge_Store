046d31e7a72973b002ae98e815af1890500d94e2
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:37:08 +0300

    Add DeviceType

--- src/main/java/com/webApp/model/DeviceType.java
+++ src/main/java/com/webApp/model/DeviceType.java
@@ -1,2 +1,14 @@
-package com.webApp.model;public class DeviceType {
+package com.webApp.model;
+
+public enum DeviceType {
+
+    /**
+     * Android device type
+     */
+    DEVICE_TYPE_ANDROID,
+
+    /**
+     * IOS device type
+     */
+    DEVICE_TYPE_IOS,
 }
