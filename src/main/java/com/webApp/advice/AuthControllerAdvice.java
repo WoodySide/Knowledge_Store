8d51155daaf179527304b48504c4c9890f0869ac
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:14:57 +0300

    Add AuthControllerAdvice class to log user authentication

--- src/main/java/com/webApp/advice/AuthControllerAdvice.java
+++ src/main/java/com/webApp/advice/AuthControllerAdvice.java
@@ -1,2 +1,190 @@
-package com.webApp.advice;public class AuthControllerAdvice {
+package com.webApp.advice;
+
+import com.webApp.exception_handling.*;
+import com.webApp.payload.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class AuthControllerAdvice {
+
+    private final MessageSource messageSource;
+
+
+    @Autowired
+    public AuthControllerAdvice(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ApiResponse processValidationError(MethodArgumentNotValidException ex, WebRequest request) {
+        BindingResult result = ex.getBindingResult();
+        List<ObjectError> allErrors = result.getAllErrors();
+        String data = processAllErrors(allErrors).stream()
+                            .collect(Collectors.joining("\n"));
+
+        return new ApiResponse(false, data, ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    /**
+     * Utility Method to generate localized message for a list of field errors
+     *
+     * @param allErrors the field errors
+     * @return the list
+     */
+    private List<String> processAllErrors(List<ObjectError> allErrors) {
+        return allErrors.stream()
+                .map(this::resolveLocalizedErrorMessage).collect(Collectors.toList());
+    }
+
+    /**
+     * Resolve localized error message. Utility method to generate a localized error
+     * message
+     *
+     * @param objectError the field error
+     * @return the string
+     */
+
+    private String resolveLocalizedErrorMessage(ObjectError objectError) {
+        Locale currentLocale = LocaleContextHolder.getLocale();
+        String localizedErrorMessage = messageSource.getMessage(objectError, currentLocale);
+        log.info(localizedErrorMessage);
+        return localizedErrorMessage;
+    }
+
+    private String resolvePathFromWebRequest(WebRequest request) {
+        try {
+            return ((ServletWebRequest) request).getRequest()
+                    .getAttribute("javax.servlet.forward.request_uri")
+                    .toString();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @ExceptionHandler(value = AppException.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ApiResponse handleAppException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = ResourceAlreadyInUseException.class)
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    @ResponseBody
+    public ApiResponse handleResourceAlreadyInUseException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = ResourceNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ApiResponse handleResourceNotFoundException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = BadRequestException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ApiResponse handleBadRequestException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = UsernameNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ApiResponse handleUsernameNotFoundException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = UserLoginException.class)
+    @ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
+    @ResponseBody
+    public ApiResponse handleUserLoginException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = BadCredentialsException.class)
+    @ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
+    @ResponseBody
+    public ApiResponse handleBadCredentialsException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = UserRegistrationException.class)
+    @ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
+    @ResponseBody
+    public ApiResponse handleUserRegistrationException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = PasswordResetLinkException.class)
+    @ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
+    @ResponseBody
+    public ApiResponse handlePasswordResetLinkException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = PasswordResetException.class)
+    @ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
+    @ResponseBody
+    public ApiResponse handlePasswordResetException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = MailSendException.class)
+    @ResponseStatus(value = HttpStatus.SERVICE_UNAVAILABLE)
+    @ResponseBody
+    public ApiResponse handleMailSendException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = InvalidTokenRequestException.class)
+    @ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
+    @ResponseBody
+    public ApiResponse handleInvalidTokenRequestException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = UpdatePasswordException.class)
+    @ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
+    @ResponseBody
+    public ApiResponse handleUpdatePasswordException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = TokenRefreshException.class)
+    @ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
+    @ResponseBody
+    public ApiResponse handleTokenRefreshException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
+
+    @ExceptionHandler(value = UserLogoutException.class)
+    @ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
+    @ResponseBody
+    public ApiResponse handleUserLogoutException(AppException ex, WebRequest request) {
+        return new ApiResponse(false, ex.getMessage(), ex.getClass().getName(), resolvePathFromWebRequest(request));
+    }
 }
