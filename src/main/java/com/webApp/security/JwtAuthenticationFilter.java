4b736f61c77fa8f5f07dae3db1d34f9a0eac7695
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:55:58 +0300

    Add JwtAuthFilter

--- src/main/java/com/webApp/security/JwtAuthenticationFilter.java
+++ src/main/java/com/webApp/security/JwtAuthenticationFilter.java
@@ -1,8 +1,11 @@
 package com.webApp.security;
 
+import com.webApp.service.CustomUserDetailsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -14,44 +17,61 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+
 
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private JwtTokenProvider tokenProvider;
+
+    @Value("${app.jwt.header}")
+    private String tokenRequestHeader;
+
+    @Value("${app.jwt.header.prefix}")
+    private String tokenRequestHeaderPrefix;
 
     @Autowired
-    private CustomerUserDetailService customerUserDetailService;
+    private JwtTokenProvider jwtTokenProvider;
 
+    @Autowired
+    private JwtTokenValidator jwtTokenValidator;
 
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    /**
+     * Filter the incoming request for a valid token in the request header
+     */
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
         try {
             String jwt = getJwtFromRequest(request);
 
-            if(StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
-                Long userId = tokenProvider.getUserIdFromJWT(jwt);
-
-
-                UserDetails userDetails = customerUserDetailService.loadUserById(userId);
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            if (StringUtils.hasText(jwt) && jwtTokenValidator.validateToken(jwt)) {
+                Long userId = jwtTokenProvider.getUserIdFromJWT(jwt);
+                UserDetails userDetails = customUserDetailsService.loadUserById(userId);
+                List<GrantedAuthority> authorities = jwtTokenProvider.getAuthoritiesFromJWT(jwt);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, jwt, authorities);
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         } catch (Exception ex) {
-            log.error("Could not set user authentication is security context", ex);
+            log.error("Failed to set user authentication in security context: ", ex);
+            throw ex;
         }
 
-        filterChain.doFilter(request,response);
-
+        filterChain.doFilter(request, response);
     }
 
+    /**
+     * Extract the token from the Authorization request header
+     */
     private String getJwtFromRequest(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7, bearerToken.length());
+        String bearerToken = request.getHeader(tokenRequestHeader);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(tokenRequestHeaderPrefix)) {
+            log.info("Extracted Token: " + bearerToken);
+            return bearerToken.replace(tokenRequestHeaderPrefix, "");
         }
         return null;
     }
