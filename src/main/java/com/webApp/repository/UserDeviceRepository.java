e173b6af8a92d27c3469f8970eeafaea28a2894f
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:49:47 +0300

    Add UserDeviceRepository

--- src/main/java/com/webApp/repository/UserDeviceRepository.java
+++ src/main/java/com/webApp/repository/UserDeviceRepository.java
@@ -1,2 +1,19 @@
-package com.webApp.repository;public class UserDeviceRepository {
+package com.webApp.repository;
+
+import com.webApp.model.UserDevice;
+import com.webApp.model.token.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDeviceRepository extends JpaRepository<UserDevice, Long> {
+
+    @Override
+    Optional<UserDevice> findById(Long id);
+
+    Optional<UserDevice> findByRefreshToken(RefreshToken refreshToken);
+
+    Optional<UserDevice> findByUserId(Long userId);
 }
