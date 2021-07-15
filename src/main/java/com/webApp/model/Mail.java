794039c1325c6efc4c4bc23b67edb74c088a223d
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:37:47 +0300

    Add Mail model class

--- src/main/java/com/webApp/model/Mail.java
+++ src/main/java/com/webApp/model/Mail.java
@@ -1,2 +1,28 @@
-package com.webApp.model;public class Mail {
+package com.webApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Mail {
+
+    private String from;
+
+    private String to;
+
+    private String subject;
+
+    private String content;
+
+    private Map<String,String> model;
+
+    public Mail() {
+        model = new HashMap<>();
+    }
 }
