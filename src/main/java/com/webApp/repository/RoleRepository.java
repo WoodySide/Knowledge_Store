20ce57f2e40ab3637fe44b6594200e455500534b
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:48:29 +0300

    Add RoleRepository

--- src/main/java/com/webApp/repository/RoleRepository.java
+++ src/main/java/com/webApp/repository/RoleRepository.java
@@ -1,14 +1,10 @@
 package com.webApp.repository;
 
 import com.webApp.model.Role;
-import com.webApp.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Optional<Role> findByName(RoleName roleName);
 }
