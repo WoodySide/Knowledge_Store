3791527af9bdeef8c4bb648d44edac5bd0de2e9a
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:38:41 +0300

    Add Role model

--- src/main/java/com/webApp/model/Role.java
+++ src/main/java/com/webApp/model/Role.java
@@ -1,26 +1,43 @@
 package com.webApp.model;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
-@Entity
-@Table(name = "roles")
-@Data
+/**
+ * The type Role. Defines the role and the list of users who are associated with that role
+ */
+
+@Entity(name = "ROLE")
 public class Role {
 
     @Id
+    @Column(name = "ROLE_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
+    @Column(name = "ROLE_NAME")
     @Enumerated(EnumType.STRING)
     @NaturalId
-    @Column(length = 60)
-    private RoleName name;
+    private RoleName role;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<User> userList = new HashSet<>();
+
+    public Role(RoleName role) {
+        this.role = role;
+    }
 
     public Role() {
+
+    }
+
+    public boolean isAdminRole() {
+        return null != this && this.role.equals(RoleName.ROLE_ADMIN);
     }
 
     public Long getId() {
@@ -31,11 +48,19 @@ public class Role {
         this.id = id;
     }
 
-    public RoleName getName() {
-        return name;
+    public RoleName getRole() {
+        return role;
     }
 
-    public void setName(RoleName name) {
-        this.name = name;
+    public void setRole(RoleName role) {
+        this.role = role;
+    }
+
+    public Set<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(Set<User> userList) {
+        this.userList = userList;
     }
 }
