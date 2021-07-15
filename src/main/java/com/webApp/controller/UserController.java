f559f4e8d0084d2c5c0f1e331682a5bb6a585186
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-15 16:23:00 +0300

    Add UserController.class

--- src/main/java/com/webApp/controller/UserController.java
+++ src/main/java/com/webApp/controller/UserController.java
@@ -1,69 +1,106 @@
 package com.webApp.controller;
 
-import com.webApp.exception_handling.NoSuchEntityException;
-import com.webApp.model.Title;
-import com.webApp.model.User;
-import com.webApp.payload.TitleResponse;
-import com.webApp.payload.UserIdentityAvailability;
-import com.webApp.payload.UserProfile;
-import com.webApp.payload.UserSummary;
+import com.webApp.event.OnUserAccountChangeEvent;
+import com.webApp.event.OnUserLogoutSuccessEvent;
+import com.webApp.exception_handling.UpdatePasswordException;
+import com.webApp.model.CustomUserDetails;
+import com.webApp.payload.ApiResponse;
+import com.webApp.payload.LogOutRequest;
+import com.webApp.payload.UpdatePasswordRequest;
 import com.webApp.repository.TitleRepository;
-import com.webApp.repository.UserRepository;
 import com.webApp.security.CurrentUser;
-import com.webApp.security.UserPrincipal;
+import com.webApp.service.AuthService;
+import com.webApp.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/user")
+@Api(value = "User Rest API", description = "Defines endpoints for the logged in user. It's secured by default")
 @Slf4j
-@Api(tags = "Users")
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final AuthService authService;
+
+    private final UserService userService;
+
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    private final TitleRepository titleRepository;
 
     @Autowired
-    private TitleRepository titleRepository;
-
-    @GetMapping(path = "/user/me")
-    public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
-        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
-        return userSummary;
+    public UserController(AuthService authService, UserService userService, ApplicationEventPublisher applicationEventPublisher, TitleRepository titleRepository) {
+        this.authService = authService;
+        this.userService = userService;
+        this.applicationEventPublisher = applicationEventPublisher;
+        this.titleRepository = titleRepository;
     }
 
-    @GetMapping(path = "/user/checkUsernameAvailability")
-    public UserIdentityAvailability checkUsernameAvailability(@RequestParam(value = "username") String username) {
-        Boolean isAvailable = !userRepository.existsByUsername(username);
-        return new UserIdentityAvailability(isAvailable);
+    /**
+     * Gets the current user profile of the logged in user
+     */
+    @GetMapping("/me")
+    @PreAuthorize("hasRole('USER')")
+    @ApiOperation(value = "Returns the current user profile")
+    public ResponseEntity getUserProfile(@CurrentUser CustomUserDetails currentUser) {
+        log.info(currentUser.getEmail() + " has role: " + currentUser.getRoles());
+        return ResponseEntity.ok("Hello. This is about me");
     }
 
-    @GetMapping(path = "/user/checkEmailAvailability")
-    public UserIdentityAvailability checkEmailAvailability(@RequestParam(value = "email") String email) {
-        Boolean isAvailable = !userRepository.existsByEmail(email);
-        return new UserIdentityAvailability(isAvailable);
+    /**
+     * Returns all admins in the system. Requires Admin access
+     */
+    @GetMapping("/admins")
+    @PreAuthorize("hasRole('ADMIN')")
+    @ApiOperation(value = "Returns the list of configured admins. Requires ADMIN Access")
+    public ResponseEntity getAllAdmins() {
+        log.info("Inside secured resource with admin");
+        return ResponseEntity.ok("Hello. This is about admins");
     }
 
-    @GetMapping(path = "/users/{userId}")
-    public UserProfile getUserProfileByUserId(@PathVariable(value = "userId") Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new NoSuchEntityException("User not found"));
+    /**
+     * Updates the password of the current logged in user
+     */
+    @PostMapping("/password/update")
+    @PreAuthorize("hasRole('USER')")
+    @ApiOperation(value = "Allows the user to change his password once logged in by supplying the correct current " +
+            "password")
+    public ResponseEntity updateUserPassword(@CurrentUser CustomUserDetails customUserDetails,
+                                             @ApiParam(value = "The UpdatePasswordRequest payload") @Valid @RequestBody UpdatePasswordRequest updatePasswordRequest) {
 
-        UserProfile userProfile = new UserProfile(user.getId(), user.getUsername(), user.getName(), user.getCreatedAt());
-
-        return userProfile;
+        return authService.updatePassword(customUserDetails, updatePasswordRequest)
+                .map(updatedUser -> {
+                    OnUserAccountChangeEvent onUserPasswordChangeEvent = new OnUserAccountChangeEvent(updatedUser, "Update Password", "Change successful");
+                    applicationEventPublisher.publishEvent(onUserPasswordChangeEvent);
+                    return ResponseEntity.ok(new ApiResponse(true, "Password changed successfully"));
+                })
+                .orElseThrow(() -> new UpdatePasswordException("--Empty--", "No such user present."));
     }
 
-    @GetMapping(path = "/users/{userId}/titles")
-    public TitleResponse getAllTitlesByUserId(@PathVariable(value = "userId") Long userId) {
-        Optional<Title> title = titleRepository.findById(userId);
+    /**
+     * Log the user out from the app/device. Release the refresh token associated with the
+     * user device.
+     */
+    @PostMapping("/logout")
+    @ApiOperation(value = "Logs the specified user device and clears the refresh tokens associated with it")
+    public ResponseEntity logoutUser(@CurrentUser CustomUserDetails customUserDetails,
+                                     @ApiParam(value = "The LogOutRequest payload") @Valid @RequestBody LogOutRequest logOutRequest) {
+        userService.logoutUser(customUserDetails, logOutRequest);
+        Object credentials = SecurityContextHolder.getContext().getAuthentication().getCredentials();
 
-        TitleResponse titleResponse = new TitleResponse(title.get().getId(), title.get().getName());
-
-        return titleResponse;
+        OnUserLogoutSuccessEvent logoutSuccessEvent = new OnUserLogoutSuccessEvent(customUserDetails.getEmail(), credentials.toString(), logOutRequest);
+        applicationEventPublisher.publishEvent(logoutSuccessEvent);
+        return ResponseEntity.ok(new ApiResponse(true, "Log out successful"));
     }
+
 }
