37755c8e67ff280680fb21b01db6ca752f6ea16d
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-16 20:13:29 +0300

    Add RoleService

--- src/main/java/com/webApp/service/RoleService.java
+++ src/main/java/com/webApp/service/RoleService.java
@@ -1,2 +1,27 @@
-package com.webApp.service;public class RoleService {
+package com.webApp.service;
+
+import com.webApp.model.Role;
+import com.webApp.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class RoleService {
+
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    /**
+     * Find all roles from the database
+     */
+    public Collection<Role> findAll() {
+        return roleRepository.findAll();
+    }
+
 }
