a22accf643e25662a04f0a29fabd1f6e7ae217bc
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-16 20:15:41 +0300

    Add Custom validation annotations

--- src/main/java/com/webApp/validation/annotation/MatchPassword.java
+++ src/main/java/com/webApp/validation/annotation/MatchPassword.java
@@ -1,2 +1,22 @@
-package com.webApp.validation.annotation;public class MatchPassword {
+package com.webApp.validation.annotation;
+
+import com.webApp.validation.validator.MatchPasswordValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = MatchPasswordValidator.class)
+@Documented
+public @interface MatchPassword {
+    String message() default "The new passwords must match";
+
+    Class<?>[] groups() default {};
+
+    boolean allowNull() default false;
+
+    Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/com/webApp/validation/validator/MatchPasswordValidator.java
+++ src/main/java/com/webApp/validation/validator/MatchPasswordValidator.java
@@ -1,2 +1,27 @@
-package com.webApp.validation.validator;public class MatchPasswordValidator {
+package com.webApp.validation.validator;
+
+import com.webApp.payload.PasswordResetRequest;
+import com.webApp.validation.annotation.MatchPassword;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class MatchPasswordValidator implements ConstraintValidator<MatchPassword, PasswordResetRequest> {
+
+    private Boolean allowNull;
+
+    @Override
+    public void initialize(MatchPassword constraintAnnotation) {
+        allowNull = constraintAnnotation.allowNull();
+    }
+
+    @Override
+    public boolean isValid(PasswordResetRequest value, ConstraintValidatorContext context) {
+        String password = value.getPassword();
+        String confirmPassword = value.getConfirmPassword();
+        if (allowNull) {
+            return null == password && null == confirmPassword;
+        }
+        return password.equals(confirmPassword);
+    }
 }
