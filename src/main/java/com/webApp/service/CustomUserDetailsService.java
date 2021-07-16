a6c2f7382b5b6ee639f0edeef8b21520c52e0a53
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-16 20:11:17 +0300

    Add CustomUserDetailsService

--- src/main/java/com/webApp/service/CustomUserDetailsService.java
+++ src/main/java/com/webApp/service/CustomUserDetailsService.java
@@ -1,2 +1,41 @@
-package com.webApp.service;public class CustomUserDetailsService {
+package com.webApp.service;
+
+
+import com.webApp.model.CustomUserDetails;
+import com.webApp.model.User;
+import com.webApp.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> dbUser = userRepository.findByEmail(email);
+        log.info("Fetched user : " + dbUser + " by " + email);
+        return dbUser.map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Couldn't find a matching user email in the database for " + email));
+    }
+
+    public UserDetails loadUserById(Long id) {
+        Optional<User> dbUser = userRepository.findById(id);
+        log.info("Fetched user : " + dbUser + " by " + id);
+        return dbUser.map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Couldn't find a matching user id in the database for " + id));
+    }
 }
