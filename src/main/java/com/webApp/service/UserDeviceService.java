b00e0eefb115888e10e74f7a4c12432bdd7a91ac
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-16 20:13:57 +0300

    Add UserDeviceService

--- src/main/java/com/webApp/service/UserDeviceService.java
+++ src/main/java/com/webApp/service/UserDeviceService.java
@@ -1,2 +1,61 @@
-package com.webApp.service;public class UserDeviceService {
+package com.webApp.service;
+
+import com.webApp.exception_handling.TokenRefreshException;
+import com.webApp.model.UserDevice;
+import com.webApp.model.token.RefreshToken;
+import com.webApp.payload.DeviceInfo;
+import com.webApp.repository.UserDeviceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDeviceService {
+
+    private final UserDeviceRepository userDeviceRepository;
+
+    @Autowired
+    public UserDeviceService(UserDeviceRepository userDeviceRepository) {
+        this.userDeviceRepository = userDeviceRepository;
+    }
+
+    /**
+     * Find the user device info by user id
+     */
+    public Optional<UserDevice> findByUserId(Long userId) {
+        return userDeviceRepository.findByUserId(userId);
+    }
+
+    /**
+     * Find the user device info by refresh token
+     */
+    public Optional<UserDevice> findByRefreshToken(RefreshToken refreshToken) {
+        return userDeviceRepository.findByRefreshToken(refreshToken);
+    }
+
+    /**
+     * Creates a new user device and set the user to the current device
+     */
+    public UserDevice createUserDevice(DeviceInfo deviceInfo) {
+        UserDevice userDevice = new UserDevice();
+        userDevice.setDeviceId(deviceInfo.getDeviceId());
+        userDevice.setDeviceType(deviceInfo.getDeviceType());
+        userDevice.setNotificationToken(deviceInfo.getNotificationToken());
+        userDevice.setRefreshActive(true);
+        return userDevice;
+    }
+
+    /**
+     * Check whether the user device corresponding to the token has refresh enabled and
+     * throw appropriate errors to the client
+     */
+    void verifyRefreshAvailability(RefreshToken refreshToken) {
+        UserDevice userDevice = findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new TokenRefreshException(refreshToken.getToken(), "No device found for the matching token. Please login again"));
+
+        if (!userDevice.getRefreshActive()) {
+            throw new TokenRefreshException(refreshToken.getToken(), "Refresh blocked for the device. Please login through a different device");
+        }
+    }
 }
