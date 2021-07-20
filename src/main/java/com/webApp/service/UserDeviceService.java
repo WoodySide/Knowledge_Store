9c1958fcc6af6fce6efc3b6fe0be791ef8a12565
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-20 10:42:37 +0300

    polish

--- src/main/java/com/webApp/service/UserDeviceService.java
+++ src/main/java/com/webApp/service/UserDeviceService.java
@@ -42,7 +42,7 @@ public class UserDeviceService {
         userDevice.setDeviceId(deviceInfo.getDeviceId());
         userDevice.setDeviceType(deviceInfo.getDeviceType());
         userDevice.setNotificationToken(deviceInfo.getNotificationToken());
-        userDevice.setRefreshActive(true);
+        userDevice.setIsRefreshActive(true);
         return userDevice;
     }
 
@@ -54,7 +54,7 @@ public class UserDeviceService {
         UserDevice userDevice = findByRefreshToken(refreshToken)
                 .orElseThrow(() -> new TokenRefreshException(refreshToken.getToken(), "No device found for the matching token. Please login again"));
 
-        if (!userDevice.getRefreshActive()) {
+        if (!userDevice.getIsRefreshActive()) {
             throw new TokenRefreshException(refreshToken.getToken(), "Refresh blocked for the device. Please login through a different device");
         }
     }
