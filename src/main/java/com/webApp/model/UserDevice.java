71c5b53479ba8b5d776a8f21a41420bba59fb11e
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-20 10:42:19 +0300

    Add @Getter, @Setter...

--- src/main/java/com/webApp/model/UserDevice.java
+++ src/main/java/com/webApp/model/UserDevice.java
@@ -2,10 +2,18 @@ package com.webApp.model;
 
 import com.webApp.model.audit.AuditModel;
 import com.webApp.model.token.RefreshToken;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity(name = "USER_DEVICE")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDevice extends AuditModel {
 
     @Id
@@ -33,74 +41,4 @@ public class UserDevice extends AuditModel {
 
     @Column(name = "IS_REFRESH_ACTIVE")
     private Boolean isRefreshActive;
-
-    public UserDevice() {
-    }
-
-    public UserDevice(Long id, User user, DeviceType deviceType, String notificationToken, String deviceId,
-                      RefreshToken refreshToken, Boolean isRefreshActive) {
-        this.id = id;
-        this.user = user;
-        this.deviceType = deviceType;
-        this.notificationToken = notificationToken;
-        this.deviceId = deviceId;
-        this.refreshToken = refreshToken;
-        this.isRefreshActive = isRefreshActive;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public DeviceType getDeviceType() {
-        return deviceType;
-    }
-
-    public void setDeviceType(DeviceType deviceType) {
-        this.deviceType = deviceType;
-    }
-
-    public String getNotificationToken() {
-        return notificationToken;
-    }
-
-    public void setNotificationToken(String notificationToken) {
-        this.notificationToken = notificationToken;
-    }
-
-    public String getDeviceId() {
-        return deviceId;
-    }
-
-    public void setDeviceId(String deviceId) {
-        this.deviceId = deviceId;
-    }
-
-    public RefreshToken getRefreshToken() {
-        return refreshToken;
-    }
-
-    public void setRefreshToken(RefreshToken refreshToken) {
-        this.refreshToken = refreshToken;
-    }
-
-    public Boolean getRefreshActive() {
-        return isRefreshActive;
-    }
-
-    public void setRefreshActive(Boolean refreshActive) {
-        isRefreshActive = refreshActive;
-    }
 }
