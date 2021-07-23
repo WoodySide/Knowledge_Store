408603c92f52379ffcf947a1a7b99c2034b14ffd
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-23 10:07:47 +0300

    Add Authorization Swagger support

--- src/main/java/com/webApp/config/SwaggerConfig.java
+++ src/main/java/com/webApp/config/SwaggerConfig.java
@@ -1,26 +1,36 @@
 package com.webApp.config;
 
-import com.webApp.security.CurrentUser;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${app.jwt.header}")
+    private static String AUTHORIZATION_HEADER;
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
     @Bean
     public Docket swagger() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .ignoredParameterTypes(CurrentUser.class)
                 .apiInfo(getApiInfo())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.webApp"))
                 .paths(PathSelectors.regex("/api.*"))
@@ -38,5 +48,16 @@ public class SwaggerConfig {
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
                 .contact(contact)
                 .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
     }
 }
