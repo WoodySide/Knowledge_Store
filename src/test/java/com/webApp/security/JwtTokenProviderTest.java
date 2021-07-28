3c18f76a051a524fa37041b7071cfe69c65a83e8
Alex Vasyaev <alexwoodyside@gmail.com>
2021-07-28 10:56:06 +0300

    Cover JWT provider and validator with tests

--- src/test/java/com/webApp/security/JwtTokenProviderTest.java
+++ src/test/java/com/webApp/security/JwtTokenProviderTest.java
@@ -1,2 +1,63 @@
-package com.webApp.security;public class JwtTokenProviderTest {
+package com.webApp.security;
+
+import com.webApp.model.CustomUserDetails;
+import com.webApp.model.Role;
+import com.webApp.model.RoleName;
+import com.webApp.model.User;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class JwtTokenProviderTest {
+
+    private static final String jwtSecret = "testSecret";
+    private static final long jwtExpiryInMs = 25000;
+
+    private JwtTokenProvider tokenProvider;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        this.tokenProvider = new JwtTokenProvider(jwtSecret,jwtExpiryInMs);
+    }
+
+    @Test
+    public void testGetUserIdFromJWT() {
+        String token = tokenProvider.generateToken(stubCustomerUser());
+        assertEquals(100, tokenProvider.getUserIdFromJWT(token).longValue());
+    }
+
+    @Test
+    public void testGetTokenExpiryFromJWT() {
+        String token = tokenProvider.generateTokenFromUserId(120L);
+        assertNotNull(tokenProvider.getTokenExpiryFromJWT(token));
+    }
+
+    @Test
+    public void testGetExpiryDuration() {
+        assertEquals(jwtExpiryInMs, tokenProvider.getExpiryDuration());
+    }
+
+    @Test
+    public void testGetAuthoritiesFromJWT() {
+        String token = tokenProvider.generateToken(stubCustomerUser());
+        assertNotNull(tokenProvider.getAuthoritiesFromJWT(token));
+    }
+
+    private CustomUserDetails stubCustomerUser() {
+        User user = new User();
+        user.setId((long) 100);
+        user.setRoles(Collections.singleton(new Role(RoleName.ROLE_ADMIN)));
+        return new CustomUserDetails(user);
+    }
 }
--- src/test/java/com/webApp/security/JwtTokenValidatorTest.java
+++ src/test/java/com/webApp/security/JwtTokenValidatorTest.java
@@ -1,2 +1,79 @@
-package com.webApp.security;public class JwtTokenValidatorTest {
+package com.webApp.security;
+
+import com.webApp.cache.LoggedOutJwtTokenCache;
+import com.webApp.event.OnUserLogoutSuccessEvent;
+import com.webApp.exception_handling.InvalidTokenRequestException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.Mockito.*;
+
+public class JwtTokenValidatorTest {
+
+    private static final String jwtSecret = "testSecret";
+    private static final long jwtExpiryInMs = 2500;
+
+    @Mock
+    private LoggedOutJwtTokenCache cache;
+
+    private JwtTokenProvider tokenProvider;
+
+    private JwtTokenValidator tokenValidator;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        this.tokenProvider = new JwtTokenProvider(jwtSecret,jwtExpiryInMs);
+        this.tokenValidator = new JwtTokenValidator(jwtSecret, cache);
+    }
+
+    @Test
+    public void testValidateTokenThrowsExceptionWhenTokenIsDamaged() {
+        String token = tokenProvider.generateTokenFromUserId(100L);
+        OnUserLogoutSuccessEvent logoutEvent = stubLogoutEvent("U1", token);
+        when(cache.getLogoutEventForToken(token)).thenReturn(logoutEvent);
+
+        thrown.expect(InvalidTokenRequestException.class);
+        thrown.expectMessage("Incorrect signature");
+        tokenValidator.validateToken(token + "-Damage");
+    }
+
+    @Test
+    public void testValidateTokenThrowsExceptionWhenTokenIsExprired() throws InterruptedException {
+        String token = tokenProvider.generateTokenFromUserId(123L);
+        TimeUnit.MILLISECONDS.sleep(jwtExpiryInMs);
+        OnUserLogoutSuccessEvent logoutEvent = stubLogoutEvent("U1", token);
+        when(cache.getLogoutEventForToken(token)).thenReturn(logoutEvent);
+    }
+
+    @Test
+    public void testValidateTokenThrowsExceptionWhenItIsPresentInTokenCache() {
+        String token = tokenProvider.generateTokenFromUserId(124L);
+        OnUserLogoutSuccessEvent logoutEvent = stubLogoutEvent("U2", token);
+        when(cache.getLogoutEventForToken(token)).thenReturn(logoutEvent);
+
+        thrown.expect(InvalidTokenRequestException.class);
+        thrown.expectMessage("Token corresponds to an already logged out user [U2]");
+        tokenValidator.validateToken(token);
+    }
+
+    @Test
+    public void testValidateTokenWorksWhenItIsNotPresentInTokenCache() {
+        String token = tokenProvider.generateTokenFromUserId(100L);
+        tokenValidator.validateToken(token);
+        verify(cache,times(1)).getLogoutEventForToken(token);
+    }
+
+    private OnUserLogoutSuccessEvent stubLogoutEvent(String email, String token) {
+        return new OnUserLogoutSuccessEvent(email,token,null);
+    }
 }
