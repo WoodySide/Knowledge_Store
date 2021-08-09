5c32065c12289c377b2db0f4437b71ba79b3bc32
Alex Vasyaev <alexwoodyside@gmail.com>
2021-08-09 16:27:59 +0300

    Polish

--- src/main/java/com/webApp/model/UserDevice.java
+++ src/main/java/com/webApp/model/UserDevice.java
@@ -2,16 +2,14 @@ package com.webApp.model;
 
 import com.webApp.model.audit.AuditModel;
 import com.webApp.model.token.RefreshToken;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity(name = "USER_DEVICE")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDevice extends AuditModel {
